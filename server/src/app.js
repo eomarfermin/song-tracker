b03dd2dc028150335d9e0ddb4a289779426ef765
eomarfermin <ellenfermin@gmail.com>
2017-09-03 21:24:50 -0400

    setting up authentication page

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,15 @@ app.use(morgan('combined'))
 app.use(bodyParse.json())
 app.use(cors())
 
-app.get('/status',(req,res) =>{
+app.get('/status', (req, res) => {
     res.send({
-        message:'hello world!'
+        message:'were are on!'
+    })
+})
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! Your user was registered! Have fun!`
     })
 })
 
